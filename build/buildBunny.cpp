5c38da20e9e4a9cbf53f87e27fb9fafe4ff0980e
NehalNN10 <nh07884@st.habib.edu.pk>
2026-05-04 15:44:42 +0500

    Create High Res bunny scene

--- build/buildBunny.cpp
+++ build/buildBunny.cpp
@@ -17,7 +17,9 @@
 void World::build(void) {
     // resolution
     int hres = 1920;
-    int vres = 1080;
+    int vres = 1920;
+    // int hres = 480;
+    // int vres = 360;
     vplane.set_hres(hres);
     vplane.set_vres(vres);
     vplane.set_bounds(Point3D(-400, 780, 0), Point3D(400, -20, 0));
@@ -38,16 +40,16 @@ void World::build(void) {
     add_light(new PointLight(Point3D(1500, 400, -200), RGBColor(0.1, 0.1, 1.0), 1.0, 0.0, 0.0)); // Blue
 
     // 4. MATERIAL LIBRARY 
-    Reflective* mat_bronze = new Reflective(0.25f, 0.50f, 0.50f, 50.0f, 0.40f, RGBColor(0.8, 0.6, 0.2));
+    // Reflective* mat_bronze = new Reflective(0.25f, 0.50f, 0.50f, 50.0f, 0.40f, RGBColor(0.8, 0.6, 0.2));
     Reflective* mat_silver = new Reflective(0.25f, 0.50f, 0.60f, 60.0f, 0.50f, RGBColor(0.8, 0.8, 0.8));
     
     Matte* mat_blue = new Matte(); mat_blue->set_cd(RGBColor(0.2, 0.4, 0.8));
     Matte* mat_pink = new Matte(); mat_pink->set_cd(RGBColor(0.9, 0.3, 0.6)); 
     
-    Phong* mat_red = new Phong(0.25f, 0.60f, 0.50f, 32.0f, RGBColor(0.8, 0.1, 0.1));
+    // Phong* mat_red = new Phong(0.25f, 0.60f, 0.50f, 32.0f, RGBColor(0.8, 0.1, 0.1));
     Phong* mat_green = new Phong(0.25f, 0.60f, 0.50f, 32.0f, RGBColor(0.1, 0.8, 0.2));
     
-    Material* bunny_mats[6] = {mat_bronze, mat_silver, mat_blue, mat_pink, mat_red, mat_green};
+    Material* bunny_mats[4] = {mat_silver, mat_blue, mat_pink, mat_green};
 
     // Chessboard Materials
     Matte* tile_light = new Matte(); tile_light->set_cd(RGBColor(0.9, 0.9, 0.9));
@@ -84,7 +86,7 @@ void World::build(void) {
                 float bx = x0 + (tile_size / 2.0f);
                 float bz = z0 + (tile_size / 2.0f);
                 
-                int rand_idx = rand() % 6; 
+                int rand_idx = rand() % 4; 
                 PLYReader::load_ply("models/bunny.ply", this, bunny_mats[rand_idx], 800.0f, Vector3D(bx, y, bz));
             }
         }
@@ -100,5 +102,5 @@ void World::build(void) {
     // 7. BVH
     std::cout << "Building BVH for massive scene...\n";
     bvh_ptr = new BVHNode(geometry, 0, geometry.size());
-    use_acceleration = true; 
+    use_acceleration = true;
 }
